bcdea115ade54ccc5b2f7ddccce3904ef134e5ed
NikzadBehnam <nikzad.behnam1992@gmail.com>
2025-08-14 08:26:36 +0200

    feat: initialize Next.js, ShadCN, Prisma and Tailwind CSS

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="py-6">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">Latest posts</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"></div>
+    </div>
+  );
+}
